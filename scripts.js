d1c0c7300e847b05cd8b4ccf26ccb5d9038f59e5
Philipp Spitzley <129886018+defPhisy@users.noreply.github.com>
2024-08-23 10:45:59 +0200

    🐛 fix calculation

--- scripts.js
+++ scripts.js
@@ -1,46 +1,43 @@
 function add(a, b) {
-  console.log("add");
   return a + b;
 }
 
 function subtract(a, b) {
-  console.log("subtract");
   return a - b;
 }
 
 function multiply(a, b) {
-  console.log("multiply");
   return a * b;
 }
 
 function divide(a, b) {
-  console.log("divide");
   return a / b;
 }
 
-// TODO: insert correct input DOM for variables
-const num1 = 3;
-const num2 = 6;
-const operator = "+";
+function operate() {
+  const n1 = +numberStack[0]; // +equation.split(/[\+\-\*\/]/)[0];
+  const op = operator; // equation.replace(/[0-9]/g, "");
+  const n2 = +numberStack[1]; // +equation.split(/[\+\-\*\/]/)[1];
 
-let result = "";
-
-function operate(num1, num2, op) {
   switch (op) {
     case "+":
-      result = add(num1, num2);
+      solution = add(n1, n2);
       break;
 
     case "-":
-      result = subtract(num1, num2);
+      solution = subtract(n1, n2);
       break;
 
-    case "*":
-      result = multiply(num1, num2);
+    case "×":
+      solution = multiply(n1, n2);
       break;
 
-    case "/":
-      result = divide(num1, num2);
+    case "÷":
+      if (n2 == 0) {
+        solution = "0 division error";
+        break;
+      }
+      solution = divide(n1, n2);
       break;
 
     default:
@@ -49,11 +46,89 @@ function operate(num1, num2, op) {
   }
 }
 
-operate(num1, num2, "+");
-console.log(result);
+function numInput(e) {
+  numberBuffer += e.target.textContent;
+  printToDisplay("main", numberBuffer);
+}
 
+function dotInput(e) {
+  if (numberBuffer.includes(".")) {
+    return;
+  }
+  numberBuffer += e.target.textContent;
+}
 
-// console.log("Add: " + (add(3, 6) === 9));
-// console.log("Subtract: " + (subtract(3, 6) === -3));
-// console.log("Multiply: " + (multiply(3, 6) === 18));
-// console.log("Divide: " + (divide(3, 6) === 0.5));
+function opInput(e) {
+  evalInput();
+  operator = e.target.textContent;
+  printToDisplay("main", operator);
+}
+
+function evalInput() {
+  if (numberBuffer) {
+    moveToNumberStack(numberBuffer);
+    numberBuffer = "";
+
+    if (numberStack.length > 1) {
+      equation = `${numberStack[0]} ${operator} ${numberStack[1]}`;
+      operate();
+      numberStack = [];
+      moveToNumberStack(solution);
+      printToDisplay("main", solution);
+      printToDisplay("top", equation);
+    }
+  }
+}
+
+function moveToNumberStack(number) {
+  numberStack.push(number);
+}
+
+function printToDisplay(position, content) {
+  const main = displayResult;
+  const top = displayHeader;
+  switch (position) {
+    case "main":
+      main.textContent = content;
+      break;
+
+    case "top":
+      top.textContent = content;
+      break;
+
+    default:
+      break;
+  }
+}
+
+function resetInput() {
+  displayHeader.textContent = "";
+  displayResult.textContent = "";
+  numberStack = [];
+  numberBuffer = "";
+  equation = "";
+}
+
+let equation = "";
+let numberBuffer = "";
+let numberStack = [];
+let operator = "";
+let solution = "";
+
+const displayHeader = document.querySelector(".display-header");
+const displayResult = document.querySelector(".display-result");
+
+const numButtons = document.querySelectorAll("button.num");
+numButtons.forEach((button) => button.addEventListener("click", numInput));
+
+const dotButton = document.querySelector(".dot");
+dotButton.addEventListener("click", dotInput);
+
+const opButtons = document.querySelectorAll("button.operator");
+opButtons.forEach((button) => button.addEventListener("click", opInput));
+
+const evalButton = document.querySelector("#eval");
+evalButton.addEventListener("click", evalInput);
+
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", resetInput);
